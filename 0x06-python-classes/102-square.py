1ba7f2317d89228fd6b4f7648eaf1bfa797ee428
OGUNYOMI <foag08@yahoo.com>
2023-12-19 13:58:59 -0800

    102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
@@ -12,8 +12,8 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, (float, int)):
-            raise TypeError("size must be a number")
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
@@ -21,20 +21,9 @@ class Square:
     def area(self):
         return self.__size * self.__size
 
-    def __eq__(self, other):
-        return self.area() == other.area()
-
-    def __ne__(self, other):
-        return self.area() != other.area()
-
-    def __gt__(self, other):
-        return self.area() > other.area()
-
-    def __ge__(self, other):
-        return self.area() >= other.area()
-
-    def __lt__(self, other):
-        return self.area() < other.area()
-
-    def __le__(self, other):
-        return self.area() <= other.area()
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
