61f40193e2beb62b5b7b086428820897206db54f
OGUNYOMI <foag08@yahoo.com>
2024-02-13 11:46:22 -0800

    4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,11 +1,2 @@
--- SQL script to create the table first_table if it doesn't exist
-
--- Connect to the MySQL server and create the table if it doesn't exist
-/* This script creates the table first_table if it doesn't already exist */
-
-USE hbtn_0c_0; -- Replace dbname with the name of the current database
-
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
+-- Creates a table called first_table with values id and name in my MySQL server.
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
