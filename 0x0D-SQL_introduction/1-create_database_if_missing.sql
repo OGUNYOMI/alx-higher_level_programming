2af32ac6c39582b43fd68b228f86479551da0bbf
OGUNYOMI <foag08@yahoo.com>
2024-02-13 08:00:31 -0800

    1-create_database_if_missing.sql

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,6 @@
+-- SQL script to create the database hbtn_0c_0 if it doesn't exist
+
+-- Connect to the MySQL server and create the database if it doesn't exist
+/* This script creates the database hbtn_0c_0 if it doesn't exist already */
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
