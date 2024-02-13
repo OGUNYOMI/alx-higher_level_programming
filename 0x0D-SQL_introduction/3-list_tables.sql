4908949c675f76090d6c8538b1a36081e03c2be8
OGUNYOMI <foag08@yahoo.com>
2024-02-13 08:46:08 -0800

    3-list_tables.sql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,6 +1,6 @@
 -- SQL script to list all tables in a specified database
 
--- Connects to the MySQL server and lists all tables in the specified database
+-- Connect to the MySQL server and list all tables in the specified database
 /* This script lists all tables in the specified database */
 
 USE `mysql`;
