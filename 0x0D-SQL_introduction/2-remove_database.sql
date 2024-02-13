56d0c1e1d9057728370ea775482f55236cbfc277
OGUNYOMI <foag08@yahoo.com>
2024-02-13 08:09:01 -0800

    2-remove_database.sql

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,6 @@
+-- SQL script to delete the database hbtn_0c_0 if it exists
+
+-- Connect to the MySQL server and delete the database if it exists
+/* This script deletes the database hbtn_0c_0 if it exists */
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
