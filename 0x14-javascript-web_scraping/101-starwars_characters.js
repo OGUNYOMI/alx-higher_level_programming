4b70f536af6f57293c7f8a8c59d12c153bf6ec0b
OGUNYOMI <foag08@yahoo.com>
2024-04-23 05:04:36 -0700

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,20 +6,38 @@ const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
+    console.error(error);
+    return;
+  }
+  
+  try {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
+    
+    const characterRequests = characters.map(character => {
+      return new Promise((resolve, reject) => {
+        request.get(character, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            try {
+              const characterData = JSON.parse(body);
+              resolve(characterData.name);
+            } catch (parseError) {
+              reject(parseError);
+            }
+          }
+        });
       });
-    }
+    });
+    
+    Promise.all(characterRequests)
+      .then(names => {
+        names.forEach(name => console.log(name));
+      })
+      .catch(error => console.error(error));
+    
+  } catch (parseError) {
+    console.error(parseError);
   }
 });
