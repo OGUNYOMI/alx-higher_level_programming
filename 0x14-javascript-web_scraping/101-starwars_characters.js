fc9909e629c156e3f8933d2804019e0fe3fd4611
OGUNYOMI <foag08@yahoo.com>
2024-04-23 05:09:27 -0700

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,7 +6,7 @@ const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.error("Error fetching movie data:", error);
     return;
   }
   
@@ -14,30 +14,32 @@ request.get(url, (error, response, body) => {
     const filmData = JSON.parse(body);
     const characters = filmData.characters;
     
-    const characterRequests = characters.map(character => {
-      return new Promise((resolve, reject) => {
-        request.get(character, (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else {
-            try {
-              const characterData = JSON.parse(body);
-              resolve(characterData.name);
-            } catch (parseError) {
-              reject(parseError);
-            }
-          }
-        });
-      });
-    });
-    
-    Promise.all(characterRequests)
+    Promise.all(characters.map(getCharacterName))
       .then(names => {
         names.forEach(name => console.log(name));
       })
-      .catch(error => console.error(error));
+      .catch(error => {
+        console.error("Error fetching character data:", error);
+      });
     
   } catch (parseError) {
-    console.error(parseError);
+    console.error("Error parsing movie data:", parseError);
   }
 });
+
+function getCharacterName(characterUrl) {
+  return new Promise((resolve, reject) => {
+    request.get(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        try {
+          const characterData = JSON.parse(body);
+          resolve(characterData.name);
+        } catch (parseError) {
+          reject(parseError);
+        }
+      }
+    });
+  });
+}
